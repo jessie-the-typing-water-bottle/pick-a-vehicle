de11fa6dcc2ee2e614314ab83496b866a52a4fd0
jessie-the-typing-water-bottle <118643022+jessie-the-typing-water-bottle@users.noreply.github.com>
2023-08-01 15:01:55 -0300

    Update pickacar.py
    Images added

--- pickacar.py
+++ pickacar.py
@@ -3,6 +3,7 @@
 
 
 import random
+from PIL import Image
 
 lst = ['car','truck','motorcycle','suv']
 
@@ -13,9 +14,17 @@ print('The vehicle you picked is a ' + (random_choice))
 
 if random_choice == 'car':
     print('🚗')
+    myImage = Image.open('caro.jpg');
+    myImage.show();
 elif random_choice == 'suv':
     print('🚙')
+    myImage = Image.open('suv.jpg');
+    myImage.show();
 elif random_choice == 'truck':
     print('🚚')
+    myImage = Image.open('truck.jpg');
+    myImage.show();
 elif random_choice == 'motorcycle':
     print('🏍')
+    myImage = Image.open('greenbike.png');
+    myImage.show();
